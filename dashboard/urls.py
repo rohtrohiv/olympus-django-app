295576cf1a183be5dc5d68907c9710fbf295ae27
Rohit Kumar <rokumar1@EXOEDGEINDIA.COM>
2025-09-26 10:00:04 -0700

    Fix routing in dashboard/urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard_explicit'),
     #path('analytics/', views.property_analytics, name='property_analytics'),
     path('analytics/', views.advanced_analytics, name='advanced_analytics'),
-    #path('sample/', views.sample_page, name='sample_page'),
+    path('analytics/query/', views.analytics_query, name='analytics_query'),
+    path('sample/', views.sample_page, name='sample_page'),
 ]
