dcdab2ff3b271ae3eea702e8da13154d6eeb928a
Rohit Kumar <rokumar1@EXOEDGEINDIA.COM>
2026-01-12 01:37:54 -0600

    update allowed host to include *

--- olympusproperty/settings.py
+++ olympusproperty/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u8=4#!bs2r+%#9vrh62(r*+752+g=-^q!-6)jk7_#m0s^3vsd&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['olympusproperty.ai','www.olympusproperty.ai']
+ALLOWED_HOSTS = ['*','olympusproperty.ai','www.olympusproperty.ai']
 
 
 # Application definition
